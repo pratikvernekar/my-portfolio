e20e9a073c01d38d438a7d6ec6df1d093cdc7b35
pratikvernekar <vernekarpratik26.pv@gmail.com>
2025-03-03 22:22:52 +0530

    3rd commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,24 @@
 import "./Projects.css";
+import s1 from "../assets/a.jpeg";
+import s2 from "../assets/a1.jpeg";
+import s3 from "../assets/a3.jpeg";
+import s4 from "../assets/a4.jpeg";
+import s5 from "../assets/a5.jpeg";
+import md from "../assets/md.webm";
+import React, { useState, useEffect } from "react";
 
 const Projects = ({ darkMode, id }) => {
+  const images = [s1, s2, s3, s4, s5];
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 2000); // Change image every 3 seconds
+
+    return () => clearInterval(interval);
+  }, []);
   const webProjects = [
     {
       title: "Web Project 1",
@@ -39,7 +57,25 @@ const Projects = ({ darkMode, id }) => {
       {/* Mobile Projects */}
       <div>
         <h2 className="category-title">Mobile Apps</h2>
-        <div className="project-list">
+        <div className="section-content">
+          <div className="text">
+            <h2></h2>
+            {/* {children} */}
+            <p className="role">MDHEALTHTRAK</p>
+            <p className="description">
+              I specialize in building high-performance, visually appealing, and
+              scalable web & mobile applications. Passionate about writing
+              clean, reusable code and constantly learning new technologies.
+            </p>
+          </div>
+
+          <div className={`ss-container`}>
+            <video autoPlay loop muted className="ss-image">
+              <source src={md} type="video/webm" />
+            </video>
+          </div>
+        </div>
+        {/* <div className="project-list">
           {mobileProjects.map((project, index) => (
             <div key={index} className="project">
               <h3>{project.title}</h3>
@@ -49,7 +85,7 @@ const Projects = ({ darkMode, id }) => {
               </a>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
